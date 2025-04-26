0fafea473ac7db1c30e6407ffb389f35cd00f15b
Justin Hunter <justin@pinata.cloud>
2025-04-25 19:27:30 -0500

    removed erroneous comments

--- src/app.ts
+++ src/app.ts
@@ -64,7 +64,7 @@ server {
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header X-Original-Host $host; // Add this line
+        proxy_set_header X-Original-Host $host;
         
         proxy_ssl_server_name on;
         proxy_ssl_protocols TLSv1.2 TLSv1.3;
@@ -101,7 +101,7 @@ server {
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header X-Original-Host $host; // Add this line
+        proxy_set_header X-Original-Host $host;
         
         proxy_ssl_server_name on;
         proxy_ssl_protocols TLSv1.2 TLSv1.3;
